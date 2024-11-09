a04d9b88b2bfa04b31039c6f3d85065d2fa246f1
menna1210 <mennaassem1210@gmail.com>
2024-11-09 04:17:00 +0200

    new commit

---
+++ even odd.js
@@ -0,0 +1,12 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function getNum(num){
+    if(num%2 === 0){
+        console.log("the number is even")
+    }
+    else(
+        console.log("the number is odd")
+
+    )
+}
+console.log(getNum(7));
