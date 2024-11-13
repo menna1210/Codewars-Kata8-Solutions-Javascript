5e9c4d9834544e6f204401ec6c82dc99dd97ea44
menna1210 <mennaassem1210@gmail.com>
2024-11-13 23:46:46 +0200

    new co\mmit

---
+++ remove-string-spaces.js
@@ -0,0 +1,6 @@
+//Write a function that removes the spaces from the string, then return the resultant string.
+
+function getremve(str){
+return str.split(" ").join("")
+}
+console.log(getremve("h e l l o w o r l d"))
